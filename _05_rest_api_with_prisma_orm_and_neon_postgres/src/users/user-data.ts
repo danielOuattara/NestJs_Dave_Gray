061455fd0bb0b20016075684a3042001bdc2a28e
danielOuattara <daniel.ouattara@gmx.com>
2024-01-11 15:38:28 +0100

    05 REST API with Prisma ORM & Neon Postgres serverless

---
+++ _05_rest_api_with_prisma_orm_and_neon_postgres/src/users/user-data.ts
@@ -0,0 +1,32 @@
+export const usersData = [
+  {
+    userId: 1,
+    name: 'Leanne Graham',
+    email: 'Sincere@april.biz',
+    role: 'INTERN',
+  },
+  {
+    userId: 2,
+    name: 'Ervin Howell',
+    email: 'Shanna@melissa.tv',
+    role: 'INTERN',
+  },
+  {
+    userId: 3,
+    name: 'Clementine Bauch',
+    email: 'Nathan@yesenia.net',
+    role: 'ENGINEER',
+  },
+  {
+    userId: 4,
+    name: 'Patricia Lebsack',
+    email: 'Julianne.OConner@kory.org',
+    role: 'ENGINEER',
+  },
+  {
+    userId: 5,
+    name: 'Chelsey Dietrich',
+    email: 'Lucio_Hettinger@annie.ca',
+    role: 'ADMIN',
+  },
+];
---
+++ _05_rest_api_with_prisma_orm_and_neon_postgres/src/users/users.controller.ts
@@ -0,0 +1,57 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Put,
+  Query,
+  ValidationPipe,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly userService: UsersService) {}
+
+  @Get()
+  findAllUsers(@Query('role') role?: 'ADMIN' | 'INTERN' | 'ENGINEER') {
+    return this.userService.findAllUsers(role);
+  }
+
+  @Get(':userId')
+  findUser(@Param('userId', ParseIntPipe) userId: number) {
+    return this.userService.findUser(userId);
+  }
+
+  @Post()
+  createUser(@Body(ValidationPipe) createUserDto: CreateUserDto) {
+    return this.userService.createUser(createUserDto);
+  }
+
+  @Patch(':userId')
+  patchUser(
+    @Param('userId', ParseIntPipe) userId: number,
+    @Body(ValidationPipe) updateUserDto: UpdateUserDto,
+  ) {
+    return this.userService.patchUser(userId, updateUserDto);
+  }
+
+  @Put(':userId')
+  updateUser(
+    @Param('userId', ParseIntPipe) userId: number,
+    @Body(ValidationPipe) createUserDto: CreateUserDto,
+  ) {
+    return this.userService.updateUser(userId, createUserDto);
+  }
+
+  @Delete(':userId')
+  deleteUser(@Param('userId', ParseIntPipe) userId: number) {
+    return this.userService.deleteUser(userId);
+  }
+}
